3d2a6830355b4e18a405d2ef805524d4dc8a81be
swayamjain1240 <swayamjain1240@gmail.com>
2026-08-13 23:03:56 +0530

    complete frontend

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -7,18 +7,33 @@ import { useAuth } from "../hooks/useAuth.js"
 import { Login } from "../pages/Login.jsx"
 import { Dashboard } from '../pages/Dashboard.jsx'
 import { Repositories } from "../pages/Repositories.jsx"
+import { PullRequests } from "../pages/PullRequests.jsx"
 import { PullRequestsDetail } from "../pages/PullRequestsDetail.jsx"
 
 
-const AppRoutes = ({user, isLoading, onLogout}) => {
+const AppRoutes = () => {
 
-    const isAuthenticated = Boolean(user);
+  const { user, loding, logout, isAuthenticated } = useAuth();
 
   return (
     <Routes>
-        <Route path='/login' element={ <LoginPlaceholder />} />
+        <Route path='/login' element={isAuthenticated ? <Navigate to="/dashboard" replace /> : <Login />} />
+
+        <Route element={<ProtectRoute isAuthenticated={isAuthenticated} isLoading={loding}> 
+              <AppLayout user={user} onLogout={logout} />
+            </ProtectRoute>}
+        />
+
+        <Route path='/' element={<Navigate to="/dashboard" replace />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/repositories' element={<Repositories />} />
+        <Route path='/pull-requests' element={<PullRequests />} />
+        <Route path='/pull-requests/:id' element={<PullRequestsDetail />} />
+        
+        <Route path='*' element={<NotFound />} />
     </Routes>
-  )
-}
+
+  );
+};
 
 export default AppRoutes
